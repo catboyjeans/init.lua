5e4bb9ae061c53ec4a2dad88f8d748e0db70b524
neko <neko@neko.machine.bsd>
2023-12-17 23:15:43 -0600

    feat: clean state
    yeeted everything

--- init.lua
+++ init.lua
@@ -1,15 +1,2 @@
--- Nvim tree configuration
-require("neko.nvim_tree_confg")
--- Plugin configs are under the after/pluggin folder
+-- Clean state configuration ;3
 
--- Remap
-require("neko.remap")
-require("neko.packer")
-require("neko.set")
-
--- Color theme thing
-vim.cmd [[colorscheme moonfly]]
-
--- Vim related config and sets
-vim.api.nvim_set_option('autoindent',true)
-vim.api.nvim_set_option('rnu',true)
